888456260315327a2521dade877daaca3acc4280
The Big Jouls <joulsjouls@protonmail.com>
2023-01-23 01:30:43 +0100

    minor changes

--- scripts/factories/media_factory.js
+++ scripts/factories/media_factory.js
@@ -5,44 +5,53 @@ class MediaFactory {
         this.title = mediaData.title;
         this.video = mediaData.video;
         this.name = photographerData.name;
-        this.mediaPath = `assets/photographers/${name}/${this.image ? this.image : this.video}`;
+        const nameOfPhotographer = this.name.split(" ");
+        const pathName = nameOfPhotographer[0].replace("-"," ");
+        this.mediaPath = `assets/photographers/${this.name}/${this.image ? this.image : this.video}`;
     }
     getMediaCardDOM() {
         //DOM éléments de media card
+        const div = document.createElement("div");
         const p = document.createElement("p");
         const media = document.createElement(this.image ? "img" : "video");
        
        //Texte inséré en éléments HTML
         p.textContent = `${this.title}`;
         
+        div.appendChild(p);
+        div.appendChild(media);
+
         //Ajout éléments créés dans le DOM
      
-        return {p, media};
+
+        return div;
+
+        
     }
 
 
-createMediaCard(currentMedias, currentPhotographer) {
-    const mediasSection = document.createElement("div");
-    mediasSection.id = "medias_section";
-    const photographerSection = document.querySelector('.photographer_section');
-    photographerSection.appendChild(mediasSection);
+//createMediaCard(currentMedias, currentPhotographer) {
+  //  const mediasSection = document.createElement("div");
+  //  mediasSection.id = "medias_section";
+   // const photographerSection = document.querySelector('.photographer_section');
+    //photographerSection.appendChild(mediasSection);
 
-    this.currentPhotographer = currentPhotographer;
+ //   this.currentPhotographer = currentPhotographer;
 
-    //Crée section pour chaque media dans le DOM
-    currentMedias.forEach((media) => {
-        const photographerMedia = new MediaFactory(media, currentPhotographer);
-        const mediaCardDOM = photographerMedia.getMediaCardDOM();
-        mediasSection.appendChild(mediaCardDOM.media);
+    //Crée section pour chaque media dans le DOM ATTENTION CA PART DANS LE photographer.js
+  //  currentMedias.forEach((media) => {
+  //      const photographerMedia = new MediaFactory(media, currentPhotographer);
+   //     const mediaCardDOM = photographerMedia.getMediaCardDOM();
+    //    mediasSection.appendChild(mediaCardDOM.media);
 
-    });
-}
+ //   });
+//}
 
-createPhotographerHeader() {
+//createPhotographerHeader() {
     //Crée header infos photographe
-    const photographHeader = document.querySelector(".photograph-header");
-    const photographerInfo = new PhotographerFactory(this.currentPhotographer);
+   // const photographHeader = document.querySelector(".photograph-header");
+   // const photographerInfo = new PhotographerFactory(this.currentPhotographer);
    // à finir
-}
+//}
 
 }
